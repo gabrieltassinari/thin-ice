5e40c77ffe8eb54b7f6d8364a1b26f97493bd08e
gabrieltassinari <100461539+gabrieltassinari@users.noreply.github.com>
2022-07-17 14:51:09 -0300

    update tilemap, main and player

--- main.lua
+++ main.lua
@@ -49,7 +49,6 @@ function love.load()
     waterFrame = 1
     keyframe = 1
 
-    haveKey = false
 end
 
 function love.update(dt)
@@ -70,39 +69,41 @@ end
 
 function love.draw()
 
-    -- Setting the font
-    love.graphics.setFont(font)
+    if level <= 12 then
 
-    -- Top interface with level, tiles and solved levels
-    love.graphics.setColor(225/255, 240/255, 255/255)
-    love.graphics.rectangle("fill", 0, 0, 456, 24)
+        -- Setting the font
+        love.graphics.setFont(font)
 
-    -- Bottom interface with reset button and points
-    love.graphics.rectangle("fill", 0, 384, 456, 24)
+        -- Top interface with level, tiles and solved levels
+        love.graphics.setColor(225/255, 240/255, 255/255)
+        love.graphics.rectangle("fill", 0, 0, 456, 24)
 
-    -- Level
-    love.graphics.setColor(6/255, 84/255, 155/255)
-    love.graphics.print("LEVEL " .. level, 36, 6)
+        -- Bottom interface with reset button and points
+        love.graphics.rectangle("fill", 0, 384, 456, 24)
 
-    -- Tiles
-    love.graphics.print(steps .. "/" .. maxSteps[level], 216, 6)
+        -- Level
+        love.graphics.setColor(6/255, 84/255, 155/255)
+        love.graphics.print("LEVEL " .. level, 30, 6)
 
-    -- Solved levels
-    love.graphics.print("SOLVED " .. level - 1, 336, 6)
+        -- Tiles
+        love.graphics.print(steps .. "/" .. maxSteps[level], 192, 6)
 
-    -- Points
-    love.graphics.print("POINTS " .. points, 312, 390)
+        -- Solved levels
+        love.graphics.print("SOLVED " .. level - 1, 312, 6)
 
-    -- Set color to default
-    love.graphics.setColor(255, 255, 255)
+        -- Points
+        love.graphics.print("POINTS " .. points, 300, 390)
+
+        -- Set color to default
+        love.graphics.setColor(255, 255, 255)
 
 
-    -- Tiles width and height
-    local width = 24
-    local height = 24
+        -- Tiles width and height
+        local width = 24
+        local height = 24
 
-    -- For each line on table
-    for i, row in ipairs(tilemap[level]) do
+        -- For each line on table
+        for i, row in ipairs(tilemap[level]) do
 
         -- For each line item
         for j, tile in ipairs(row) do
@@ -133,102 +134,14 @@ function love.draw()
             end
 
         end
+        
+        -- Draw player
+        player:draw()
+    else
+        -- LOAD FINAL SCREEN
     end
-
-    -- Draw player
-    love.graphics.draw(player.image, (player.tile_x - 1) * width, (player.tile_y - 0) * height)
-    
 end
 
 function love.keypressed(key)
-
-    local x = player.tile_x
-    local y = player.tile_y
-
-    -- Player moves
-    if key == "left" then
-        x = x - 1
-    elseif key == "right" then
-        x = x + 1
-    elseif key == "up" then
-        y = y - 1
-    elseif key == "down" then
-        y = y + 1
-    end
-
-    if isEmpty(x, y) then
-
-        points = points + 1
-        steps = steps + 1
-
-        -- If player moves to snow
-        if tilemap[level][y][x] == 6 then
-            tilemap[level][y][x] = 0
-
-        -- If player moves to money
-        elseif tilemap[level][y][x] == 3 then
-            tilemap[level][y][x] = 0
-            points = points + 100
-        
-        -- If player moves to carpet
-        elseif tilemap[level][y][x] == 9 then
-            tilemap[level][y][x] = 6
-
-        -- If player moves to lock
-        elseif tilemap[level][y][x] == 11 then
-            tilemap[level][y][x] = 0
-
-        -- If player moves to key
-        elseif tilemap[level][y][x] == 13 then
-            tilemap[level][y][x] = 0
-        end
-        
-        -- Update player position
-        player.tile_x = x
-        player.tile_y = y
-
-        -- If player moves to Warp
-        if tilemap[level][y][x] == 8 then
-            
-            -- Points per level
-            points = points + (level * 14) + 10
-
-            -- Check if have money on the next level
-            hasMoney = checkSteps()
-
-            -- Player go to next level
-            level = level + 1
-            
-            -- Reset steps counter
-            steps = 0
-
-        end
-    end
-end
-
-function isEmpty(x, y)
-
-    -- If tile is money, snow, warp or carpet
-    if tilemap[level][y][x] == 3 or tilemap[level][y][x] == 6 or
-       tilemap[level][y][x] == 8 or tilemap[level][y][x] == 9 then
-        return true
-    
-    -- If tile is lock
-    elseif tilemap[level][y][x] == 11 and haveKey == true then
-        return true
-    
-    -- If tile is key
-    elseif tilemap[level][y][x] == 13 then
-        haveKey = true
-        return true
-    end
-
-end
-
-function checkSteps()
-    if steps == maxSteps[level] then
-        return true
-    else
-        return false
-    end
+    player:keyPressed(key)
 end
--- player.lua
+++ player.lua
@@ -4,4 +4,106 @@ function Player:new(x, y)
     Player.super.new(self, x, y, "sprites/player.png")
     self.tile_x = x
     self.tile_y = y
+    self.haveKey = false
+end
+
+function Player:draw()
+    love.graphics.draw(player.image, (player.tile_x - 1) * 24, player.tile_y * 24)
+end
+
+function Player:keyPressed(key)
+
+    local x = player.tile_x
+    local y = player.tile_y
+
+    -- Player moves
+    if key == "left" then
+        x = x - 1
+    elseif key == "right" then
+        x = x + 1
+    elseif key == "up" then
+        y = y - 1
+    elseif key == "down" then
+        y = y + 1
+    end
+
+    -- If next position is valid
+    if isEmpty(x, y) then
+
+        points = points + 1
+        steps = steps + 1
+
+        -- If player moves to snow
+        if tilemap[level][y][x] == 6 then
+            tilemap[level][y][x] = 0
+
+        -- If player moves to money
+        elseif tilemap[level][y][x] == 3 then
+            tilemap[level][y][x] = 0
+            points = points + 100
+        
+        -- If player moves to carpet
+        elseif tilemap[level][y][x] == 9 then
+            tilemap[level][y][x] = 6
+
+        -- If player moves to lock
+        elseif tilemap[level][y][x] == 11 then
+            tilemap[level][y][x] = 0
+
+        -- If player moves to key
+        elseif tilemap[level][y][x] == 13 then
+            tilemap[level][y][x] = 0
+        end
+        
+        -- Update player position
+        player.tile_x = x
+        player.tile_y = y
+
+        -- If player moves to Warp
+        if tilemap[level][y][x] == 8 then
+            
+            -- Save current position
+            position_x = x
+            position_y = y
+
+            -- Points per level
+            points = points + (level * 14) + 10
+
+            -- Check if have money on the next level
+            hasMoney = checkSteps()
+
+            -- Player go to next level
+            level = level + 1
+            
+            -- Reset steps counter
+            steps = 0
+
+        end
+    end
+end
+
+function isEmpty(x, y)
+
+    -- If tile is money, snow, warp or carpet
+    if tilemap[level][y][x] == 3 or tilemap[level][y][x] == 6 or
+       tilemap[level][y][x] == 8 or tilemap[level][y][x] == 9 then
+        return true
+
+    -- If tile is lock
+    elseif tilemap[level][y][x] == 11 and player.haveKey == true then
+        return true
+    
+    -- If tile is key
+    elseif tilemap[level][y][x] == 13 then
+        player.haveKey = true
+        return true
+    end
+end
+
+function checkSteps()
+    if steps == maxSteps[level] then
+        return true
+    else
+        return false
+    end
 end
